31f8f5bbbe46f91fadb58b2476cb4bfca0a9fd01
Abhideep Singh <abhi@dothat.in>
2014-02-20 19:17:12 +0530

    Initial checkin for Datasource interface and static implementation

---
+++ src/java/com/sarality/app/datasource/DataSource.java
@@ -0,0 +1,26 @@
+package com.sarality.app.datasource;
+
+import java.util.List;
+
+/**
+ * Interface for all classes that provide data a Component
+ * 
+ * @author abhideep@dothat.in (Abhideep Singh)
+ */
+public interface DataSource<T> {
+
+  /**
+   * @return true if the data set is of a fixed size.
+   */
+  public boolean isStaticDataSet();
+
+  /**
+   * Loads the data that needs to be displayed
+   */
+  public void loadData();
+
+  /**
+   * @return List of data that needs to be displayed
+   */
+  public List<T> getData();
+}
