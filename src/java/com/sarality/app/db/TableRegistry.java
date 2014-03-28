6118921244d8202c34b607ac2864a3ccd2a713a0
Abhideep Singh <abhi@dothat.in>
2014-03-28 18:47:52 +0530

    Added resgitry to allow for singleton access to tables

---
+++ src/java/com/sarality/app/db/TableRegistry.java
@@ -0,0 +1,16 @@
+package com.sarality.app.db;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TableRegistry {
+  private final Map<String, Table<?>> registry = new HashMap<String, Table<?>>();
+  
+  public void register(String name, Table<?> table) {
+    registry.put(name, table);
+  }
+
+  public Table<?> getTable(String name) {
+    return registry.get(name);
+  }
+}
