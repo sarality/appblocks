68d98cf974cdcc69a7f16650d39c7af6f80a9e9a
Abhideep Singh <abhi@dothat.in>
2014-04-07 17:01:59 +0530

    Abstract implementations of datastores

---
+++ src/java/com/sarality/app/datastore/DataStore.java
@@ -0,0 +1,48 @@
+package com.sarality.app.datastore;
+
+import java.util.List;
+
+import com.sarality.app.data.DataObject;
+import com.sarality.app.datastore.extractor.CursorDataExtractor;
+
+/**
+ * Interface for all classes that implement the ability to fetch DataObjects from a store
+ * defined by a set of Columns.
+ * <p>
+ * 
+ * {@see WritableDatastore, AbstractDatastore, AbstractWritableDatastore}
+ * 
+ * @author abhideep@ (Abhideep Singh)
+ *
+ * @param <T> The type of DataObject return the by the DataStore.
+ */
+public interface DataStore<T extends DataObject<T>> {
+
+  /**
+   * Open a DataStore for further operations
+   */
+  public void open();
+
+  /**
+   * Close the DataStore and perform necessary cleanups
+   */
+  public void close();
+
+  /**
+   * @return {@code List} of {@code Column}s defined for the DataStore.
+   */
+  public List<Column> getColumns();
+
+  /**
+   * Returns a {@code List} of {@code DataObject}s that match the given query.
+   * 
+   * @param query The query to run on the DataStore.
+   * @return  {@code List} of {@code DataObject}s matching the query.
+   */
+  public List<T> query(Query query);
+
+  /**
+   * @return The class that convert the cursor returned by the DataStore into a DataObject.
+   */
+  public CursorDataExtractor<T> getCursorDataExtractor();
+}
---
+++ src/java/com/sarality/app/datastore/WritableDataStore.java
@@ -0,0 +1,36 @@
+package com.sarality.app.datastore;
+
+import com.sarality.app.data.DataObject;
+import com.sarality.app.datastore.populator.ContentValuesPopulator;
+
+/**
+ * An interface for all classes that can not only read by insert and update items in a {@code DataStore}.
+ * 
+ * @author abhideep@ (Abhideep Singh)
+ *
+ * @param <T> The type of {@link DataObject} needed to create the item in the store.
+ * @param <I> The type of ID returned when creating the item in the store.
+ */
+public interface WritableDataStore<T extends DataObject<T>, I> extends DataStore<T> {
+
+  /**
+   * Create an item in the {@code DataStore}.
+   *
+   * @param data The data for the item that needs to be created.
+   * @return The id for the item that was created.
+   */
+  public I create(T data);
+
+  /**
+   * Delete the item(s) for the given query.
+   * 
+   * @param query Query to define the set of items that need to be deleted.
+   */
+  public void delete(Query query);
+
+  /**
+   * @return A class that populates values in the {@code ContentValues} needed to insert
+   * or update data in a data store.
+   */
+  public ContentValuesPopulator<T> getContentValuesPopulator();
+}
