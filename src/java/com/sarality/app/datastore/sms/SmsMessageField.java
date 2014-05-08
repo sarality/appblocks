2f14edfe35cf76cfa792612082d6bac5eb805893
Abhideep Singh <abhi@dothat.in>
2014-05-08 16:20:22 +0530

    FieldData based SmsMessage

--- src/java/com/sarality/app/datastore/sms/SmsMessageField.java
+++ src/java/com/sarality/app/datastore/sms/SmsMessageField.java
@@ -4,8 +4,11 @@ import com.sarality.app.data.Field;
 
 public enum SmsMessageField implements Field {
   MESSAGE_ID,
+  THREAD_ID,
   ADDRESS,
-  BODY;
+  BODY,
+  MESSAGE_TYPE, IS_READ, SENT_DATE, RECEIVED_DATE, RECIPIENT,
+  ;
 
   @Override
   public String getName() {
