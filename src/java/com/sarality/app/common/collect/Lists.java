b70a4ce8e881a8230082f30043f96216ac2711f4
Abhideep Singh <abhi@dothat.in>
2014-06-11 12:14:49 +0530

    Utility classes to easily build Lists and Maps

--- src/java/com/sarality/app/common/collect/Lists.java
+++ src/java/com/sarality/app/common/collect/Lists.java
@@ -3,8 +3,22 @@ package com.sarality.app.common.collect;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility class to create List objects without having to
+ * specify Template arguments.
+ * <p>
+ * Inspired from Google's Guava Library.
+ * 
+ * @author abhidep@ (Abhideep Singh)
+ */
 public class Lists {
 
+  /**
+   * Create an ArrayList with the given elements.
+   * 
+   * @param elements Elements to include in the list.
+   * @return ArrayList with given elements.
+   */
   public static <T> List<T> of(T... elements) {
     List<T> list = new ArrayList<T>();
     for (T element : elements) {
