4b33a034a61810007c21c10a2036e1c8350cb951
Abhideep Singh <abhi@dothat.in>
2014-07-07 15:31:07 +0530

    Added ability to compare a FilterValue with a given value.
    This is in preparation for an InMemory Table where we can run filters that way.

--- src/java/com/sarality/app/datastore/query/DateFilterValue.java
+++ src/java/com/sarality/app/datastore/query/DateFilterValue.java
@@ -9,8 +9,7 @@ import com.sarality.app.datastore.ColumnDataType;
 import com.sarality.app.datastore.ColumnFormat;
 
 /**
- * Converts a Date value into a String, based on the Column Spec so that it can be used
- * in the query to a data source.
+ * Converts a Date value into a String, based on the Column Spec so that it can be used in the query to a data source.
  * 
  * @author abhideep (Abhideep Singh)
  */
@@ -18,7 +17,6 @@ class DateFilterValue extends FilterValue<Date> {
   private final SimpleDateFormat defaultFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
   private final SimpleDateFormat dateAsIntegerFormatter = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
 
-  
   DateFilterValue(Date value) {
     super(value);
   }
@@ -27,8 +25,7 @@ class DateFilterValue extends FilterValue<Date> {
   String getStringValue(Column column) {
     ColumnDataType dataType = column.getSpec().getDataType();
     if (dataType != ColumnDataType.DATETIME) {
-      throw new IllegalArgumentException("Cannot set filter value of type Date on column with data type "
-          + dataType);
+      throw new IllegalArgumentException("Cannot set filter value of type Date on column with data type " + dataType);
     }
 
     ColumnFormat format = column.getSpec().getFormat();
@@ -40,5 +37,10 @@ class DateFilterValue extends FilterValue<Date> {
       return dateAsIntegerFormatter.format(getValue());
     }
     return defaultFormatter.format(getValue());
+  }
+
+  @Override
+  int compareTo(Date value) {
+    return getValue().compareTo(value);
   }
 }
