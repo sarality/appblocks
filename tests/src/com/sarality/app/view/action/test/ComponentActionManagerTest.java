d4f2d95143af541870d9d5873f413bde793d4a43
Sunayna Uberoy <sunayna@dothat.in>
2014-07-28 16:11:38 +0530

    Cleanup code

--- tests/src/com/sarality/app/view/action/test/ComponentActionManagerTest.java
+++ tests/src/com/sarality/app/view/action/test/ComponentActionManagerTest.java
@@ -21,66 +21,48 @@ import com.sarality.app.view.action.ViewAction;
  */
 public class ComponentActionManagerTest extends BaseUnitTest {
 
-  private ViewAction action1;
-  private ViewAction action2;
-  private ViewAction action3;
-  private ViewAction action4;
-  private ViewAction action5;
-  private ViewAction action6;
-  private ViewAction action7;
-  private ViewAction action8;
+  private ViewAction view1Action1;
+  private ViewAction listView2Action1;
+  private ViewAction view1Action2;
+  private ViewAction listView2Action2;
+  private ViewAction view3Action1;
+  private ViewAction view3Action2;
+  private ViewAction view3Action3;
+  private ViewAction view1Action3;
   List<ViewAction> actionList;
 
   @Override
   public void setUp() {
-    try {
-      super.setUp();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    action1 = mock(ViewAction.class);
-    action2 = mock(ViewAction.class);
-    action3 = mock(ViewAction.class);
-    action4 = mock(ViewAction.class);
-    action5 = mock(ViewAction.class);
-    action6 = mock(ViewAction.class);
-    action7 = mock(ViewAction.class);
-    action8 = mock(ViewAction.class);
+    super.setUp();
+
+    // view1Action1, view1Action2, view1Action3 on view1
+    // listViewAction1, listViewAction2 on listView
+    // view2Action1, view2Action2, view2Action3 on view3
+
+    view1Action1 = createMockAction(1, TriggerType.CLICK);
+    listView2Action1 = createMockAction(2, TriggerType.CLICK_LIST_ITEM);
+    view1Action2 = createMockAction(1, TriggerType.LONG_CLICK);
+    listView2Action2 = createMockAction(2, TriggerType.LONG_CLICK_LIST_ITEM);
+    view3Action1 = createMockAction(3, TriggerType.TOUCH);
+    view3Action2 = createMockAction(3, TriggerType.TOUCH_DOWN);
+    view3Action3 = createMockAction(3, TriggerType.TOUCH_UP);
+    view1Action3 = createMockAction(1, TriggerType.CLICK);
+  }
+
+  private ViewAction createMockAction(int viewId, TriggerType trigger) {
+    ViewAction action = mock(ViewAction.class);
+    when(action.getTriggerType()).thenReturn(trigger);
+    when(action.getViewId()).thenReturn(viewId);
+    return action;
   }
 
   private void setupActionList() {
-    actionList = Lists.of(action1, action2, action3, action4, action5, action6, action7, action8);
-  }
-
-  private void setupTriggerType() {
-    when(action1.getTriggerType()).thenReturn(TriggerType.CLICK);
-    when(action2.getTriggerType()).thenReturn(TriggerType.CLICK_LIST_ITEM);
-    when(action3.getTriggerType()).thenReturn(TriggerType.LONG_CLICK);
-    when(action4.getTriggerType()).thenReturn(TriggerType.LONG_CLICK_LIST_ITEM);
-    when(action5.getTriggerType()).thenReturn(TriggerType.TOUCH);
-    when(action6.getTriggerType()).thenReturn(TriggerType.TOUCH_DOWN);
-    when(action7.getTriggerType()).thenReturn(TriggerType.TOUCH_UP);
-    when(action8.getTriggerType()).thenReturn(TriggerType.CLICK);
-  }
-
-  private void setupActionGetViewId() {
-    // ACTION1, ACTION3, ACTION8 on view1
-    // ACTION2, ACTION4 on listView
-    // ACTION5, ACTION6, ACTION7 on view3
-
-    when(action1.getViewId()).thenReturn(1);
-    when(action2.getViewId()).thenReturn(2);
-    when(action3.getViewId()).thenReturn(1);
-    when(action4.getViewId()).thenReturn(2);
-    when(action5.getViewId()).thenReturn(3);
-    when(action6.getViewId()).thenReturn(3);
-    when(action7.getViewId()).thenReturn(3);
-    when(action8.getViewId()).thenReturn(1);
+    actionList = Lists.of(view1Action1, listView2Action1, view1Action2, listView2Action2, view3Action1, view3Action2,
+        view3Action3, view1Action3);
   }
 
   public void testComponentActionManager() {
     setupActionList();
-    setupActionGetViewId();
     ComponentActionManager actionManager = new ComponentActionManager(actionList);
     assertNotNull(actionManager);
   }
@@ -100,34 +82,30 @@ public class ComponentActionManagerTest extends BaseUnitTest {
     listView.setId(2);
 
     setupActionList();
-    setupActionGetViewId();
-    setupTriggerType();
 
     when(layout.findViewById(1)).thenReturn(view1);
     when(layout.findViewById(2)).thenReturn(listView);
     when(layout.findViewById(3)).thenReturn(view3);
 
     ComponentActionManager actionManager = new ComponentActionManager(actionList);
+    // No exceptions should be thrown
     actionManager.setupActions(layout);
   }
-  
-  public void testSetUpActions_Exception(){
+
+  public void testSetUpActions_Exception() {
     Context context = getInstrumentation().getTargetContext();
     View layout = mock(View.class);
     View view1 = new View(context);
     view1.setId(13);
 
-    actionList = Lists.of(action1);
-    setupActionGetViewId();
-    setupTriggerType();
+    actionList = Lists.of(view1Action1);
 
     when(layout.findViewById(1)).thenReturn(view1);
-    
+
     ComponentActionManager actionManager = new ComponentActionManager(actionList);
-    try{
+    try {
       actionManager.setupActions(layout);
-    }
-    catch(IllegalArgumentException e){
+    } catch (IllegalArgumentException e) {
       // Do nothing
     }
   }
