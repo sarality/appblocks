1f7ab3b0976483d62c9f15a2eb89b323ea51e3d6
Sunayna Uberoy <sunayna@dothat.in>
2014-07-29 10:41:04 +0530

    Minor code review comments

--- tests/src/com/sarality/app/view/action/test/CompositeViewActionTest.java
+++ tests/src/com/sarality/app/view/action/test/CompositeViewActionTest.java
@@ -6,8 +6,6 @@ import static org.mockito.Mockito.when;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
-import android.util.Log;
-
 import com.sarality.app.view.action.CompositeViewAction;
 import com.sarality.app.view.action.TriggerType;
 import com.sarality.app.view.action.ViewAction;
@@ -19,78 +17,75 @@ import com.sarality.app.view.action.ViewAction;
  */
 public class CompositeViewActionTest extends BaseUnitTest {
 
-  private static final String TAG = null;
-
   public void testCompositeViewAction() {
     CompositeViewAction viewAction = new CompositeViewAction(1, TriggerType.CLICK);
     assertNotNull(viewAction);
-    viewAction = new CompositeViewAction(1, TriggerType.CLICK_LIST_ITEM);
-    assertNotNull(viewAction);
-    viewAction = new CompositeViewAction(1, TriggerType.LONG_CLICK);
-    assertNotNull(viewAction);
-    viewAction = new CompositeViewAction(1, TriggerType.LONG_CLICK_LIST_ITEM);
-    assertNotNull(viewAction);
-    viewAction = new CompositeViewAction(1, TriggerType.TOUCH);
-    assertNotNull(viewAction);
-    viewAction = new CompositeViewAction(1, TriggerType.TOUCH_DOWN);
-    assertNotNull(viewAction);
-    viewAction = new CompositeViewAction(1, TriggerType.TOUCH_UP);
-    assertNotNull(viewAction);
+    assertEquals(TriggerType.CLICK, viewAction.getTriggerType());
+    assertEquals(1, viewAction.getViewId());
+  }
+
+  private ViewAction createAction(int viewId, TriggerType triggerType) {
+    ViewAction action = mock(ViewAction.class);
+    when(action.getViewId()).thenReturn(viewId);
+    when(action.getTriggerType()).thenReturn(triggerType);
+    when(action.performAction(null, null, null)).thenReturn(true);
+    return action;
   }
 
   public void testRegisterAction() {
     CompositeViewAction viewAction = new CompositeViewAction(1, TriggerType.CLICK);
-    ViewAction action1 = mock(ViewAction.class);
-    ViewAction action2 = mock(ViewAction.class);
-    ViewAction action3 = mock(ViewAction.class);
-
-    when(action1.getViewId()).thenReturn(1);
-    when(action2.getViewId()).thenReturn(1);
-    when(action1.getTriggerType()).thenReturn(TriggerType.CLICK);
-    when(action2.getTriggerType()).thenReturn(TriggerType.CLICK);
-    when(action2.performAction(null, null, null)).thenReturn(true);
-    when(action1.performAction(null, null, null)).thenReturn(true);
-    when(action2.performAction(null, null, null)).thenReturn(true);
+    ViewAction clickAction1 = createAction(viewAction.getViewId(), viewAction.getTriggerType());
+    ViewAction clickAction2 = createAction(viewAction.getViewId(), viewAction.getTriggerType());
+    ViewAction nullAction = mock(ViewAction.class);
 
     // create inOrder object passing any mocks that need to be verified in order
-    InOrder inOrder = Mockito.inOrder(action1, action2);
- 
-    viewAction.registerAction(action1);
-    viewAction.registerAction(action2);
+    InOrder inOrder = Mockito.inOrder(clickAction1, clickAction2);
+
+    viewAction.registerAction(clickAction1);
+    viewAction.registerAction(clickAction2);
     boolean result = viewAction.doAction(null, null, null);
 
     assertEquals(result, true);
-    inOrder.verify(action1).performAction(null, null, null);
-    inOrder.verify(action2).performAction(null, null, null);
-    Mockito.verifyZeroInteractions(action3);
- }
+    inOrder.verify(clickAction1).performAction(null, null, null);
+    inOrder.verify(clickAction2).performAction(null, null, null);
+    Mockito.verifyZeroInteractions(nullAction);
+  }
 
   public void testRegisterAction_IllegalArgument() {
     CompositeViewAction viewAction = new CompositeViewAction(1, TriggerType.CLICK);
-    ViewAction action1 = mock(ViewAction.class);
-    ViewAction action2 = mock(ViewAction.class);
-
-    when(action1.getViewId()).thenReturn(2);
-    when(action2.getViewId()).thenReturn(1);
-    when(action1.getTriggerType()).thenReturn(TriggerType.CLICK);
-    when(action2.getTriggerType()).thenReturn(TriggerType.TOUCH);
+    ViewAction clickActionView2 = createAction(2, TriggerType.CLICK);
+    ViewAction touchActionView1 = createAction(1, TriggerType.TOUCH);
 
     try {
-      viewAction.registerAction(action1);
-      viewAction.registerAction(action2);
+      viewAction.registerAction(clickActionView2);
+      viewAction.registerAction(touchActionView1);
     } catch (IllegalArgumentException e) {
-      Log.d(TAG,"Exception thrown...");
+      // Do nothing
+    } catch (Exception e) {
+      fail("Exception other than expected IllegalArgumentException thrown");
+      e.printStackTrace();
     }
 
-    when(action1.getViewId()).thenReturn(1);
-    
+    // clickActionView2 should not get registered as the compositeAction View and action view do not match
+    Mockito.verify(clickActionView2, Mockito.times(2)).getViewId();
+    // Exception should be thrown before registration for this item happens
+    Mockito.verifyZeroInteractions(touchActionView1);
+
+    ViewAction clickActionView1 = createAction(1, TriggerType.CLICK);
+
     try {
-      viewAction.registerAction(action1);
-      viewAction.registerAction(action2);
+      viewAction.registerAction(clickActionView1);
+      viewAction.registerAction(touchActionView1);
     } catch (IllegalArgumentException e) {
       // do nothing
+    } catch (Exception e) {
+      fail("Exception other than expected IllegalArgumentException thrown");
+      e.printStackTrace();
     }
-    Mockito.verify(action1, Mockito.times(3)).getViewId();
-    Mockito.verify(action2, Mockito.times(2)).getViewId();
+    // clickActionView1 gets registered
+    // touchActionView1 should not get registered
+    // IllegalArgument exception would be thrown
+    Mockito.verify(clickActionView1, Mockito.times(1)).getViewId();
+    Mockito.verify(touchActionView1, Mockito.times(2)).getViewId();
   }
 }
