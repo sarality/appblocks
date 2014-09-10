45c99a9da67559cc123f8098f0e142bd906713e5
Abhideep Singh <abhi@dothat.in>
2014-09-10 11:05:27 +0530

    STart using new Field class

--- src/java/com/sarality/app/datastore/sms/SmsMessageField.java
+++ src/java/com/sarality/app/datastore/sms/SmsMessageField.java
@@ -1,17 +1,36 @@
 package com.sarality.app.datastore.sms;
 
-import com.sarality.app.data.Field;
+import com.sarality.app.data.field.Field;
 
+/**
+ * Fields for {@link SmsMessage}.
+ * 
+ * @author abhideep@ (Abhideep Singh)
+ */
 public enum SmsMessageField implements Field {
-  MESSAGE_ID,
-  THREAD_ID,
-  ADDRESS,
-  BODY,
-  MESSAGE_TYPE, IS_READ, SENT_DATE, RECEIVED_DATE, RECIPIENT,
-  ;
+  MESSAGE_ID(DataType.LONG),
+  THREAD_ID(DataType.LONG),
+  ADDRESS(DataType.STRING),
+  BODY(DataType.STRING),
+  MESSAGE_TYPE(DataType.ENUM),
+  IS_READ(DataType.BOOLEAN),
+  SENT_DATE(DataType.DATE),
+  RECEIVED_DATE(DataType.DATE),
+  RECIPIENT(DataType.STRING);
+
+  private DataType dataType;
+
+  private SmsMessageField(DataType dataType) {
+    this.dataType = dataType;
+  }
 
   @Override
   public String getName() {
     return name();
+  }
+
+  @Override
+  public DataType getDataType() {
+    return dataType;
   }
 }
