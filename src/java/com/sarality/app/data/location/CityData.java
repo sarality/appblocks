9622ba2baf12444e19f19080a619b7224f005419
Abhideep Singh <abhi@dothat.in>
2014-12-15 12:34:18 +0530

    Enum Data Object for City

--- src/java/com/sarality/app/data/location/CityData.java
+++ src/java/com/sarality/app/data/location/CityData.java
@@ -1,8 +1,9 @@
 package com.sarality.app.data.location;
 
-import com.sarality.app.data.DataObject;
-import com.sarality.app.data.DataTransformer;
+import com.sarality.app.data.EnumDataObject;
 import com.sarality.app.data.image.AppImage;
+import com.sarality.app.datastore.EnumDataObjectComposer;
+import com.sarality.app.datastore.EnumDataObjectLoader;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ import java.util.List;
  *
  * @author abhideep@ (Abhideep Singh)
  */
-public class CityData implements DataObject<CityData> {
+public class CityData implements EnumDataObject<City, CityData> {
   private final Long cityId;
   private final City city;
   private final Country country;
@@ -35,11 +36,44 @@ public class CityData implements DataObject<CityData> {
     return new CityDataBuilder();
   }
 
-  public static class CityDataGenerator implements DataTransformer<City, CityData> {
+  @Override public City getEnumData() {
+    return city;
+  }
 
-    @Override public CityData transform(City city) {
-      // TODO(abhideep): Add support for Country
-      return new CityData(null, city, null, null);
+  public City getCity() {
+    return city;
+  }
+
+  public Country getCountry() {
+    return country;
+  }
+
+  public List<AppImage> getImageList() {
+    return imageList;
+  }
+
+  /**
+   * Enum for components of CityData
+   * <p/>
+   * TODO(abhideep): Look into potentially using Fields here in the Future.
+   */
+  public enum Component {
+    IMAGE,
+  }
+
+  /**
+   * Composes an AirlineData object for a given Airline and Map of Associated Data Providers.
+   *
+   * @author abhideep@ (Abhideep Singh)
+   */
+  public static class Composer implements EnumDataObjectComposer<City, CityData> {
+
+    @Override
+    public CityData compose(City city, EnumDataObjectLoader<City, CityData> loader) {
+
+      List<AppImage> imageList = loader.getAssociatedData(city, Component.IMAGE.name());
+      return new CityData(null, city, city.getCountry(), imageList);
     }
   }
+
 }
