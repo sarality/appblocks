ec530cb66fcbf323127c1e2016a8d8c6e494c14a
Abhideep Singh <abhi@dothat.in>
2015-02-06 14:25:15 +0530

    Convert RefreshableFragment to an interface. Much easier to deal with interface here when you don't have to deal with multiple inheritance.

--- src/java/com/sarality/app/view/fragment/RefreshableFragment.java
+++ src/java/com/sarality/app/view/fragment/RefreshableFragment.java
@@ -1,15 +1,14 @@
 package com.sarality.app.view.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 
 /**
- * Fragment that has the capability of being refreshed with a new set of arguments
+ * Interface for Fragments that have the capability of being refreshed with a new set of arguments.
  *
  * @author sunayna@ (Sunayna Uberoy)
  */
-public abstract class RefreshableFragment extends Fragment {
+public interface RefreshableFragment {
 
-  public abstract void refresh(Bundle bundle);
+  public void refresh(Bundle bundle);
 }
 
