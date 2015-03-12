9d1955a83d6a6ce872aba6b811310ea2561a100f
Abhideep Singh <abhi@dothat.in>
2015-03-12 12:16:42 +0530

    Base Extractor no longer needs to be aware of how messages are parsed.

--- src/java/com/sarality/app/extract/SmsMessageEventExtractor.java
+++ src/java/com/sarality/app/extract/SmsMessageEventExtractor.java
@@ -3,8 +3,6 @@ package com.sarality.app.extract;
 import com.sarality.app.datastore.sms.SmsMessage;
 
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Base class for all Extractors that extract events from an SMS Message.
@@ -14,22 +12,12 @@ import java.util.regex.Pattern;
 public abstract class SmsMessageEventExtractor<E> implements MessageEventExtractor<SmsMessage, E> {
 
   private final Set<String> senderIds;
-  private final Pattern messagePattern;
 
-  public SmsMessageEventExtractor(Set<String> senderIdSet, String messagePattern) {
+  public SmsMessageEventExtractor(Set<String> senderIdSet) {
     this.senderIds = senderIdSet;
-    this.messagePattern = Pattern.compile(messagePattern);
   }
 
   protected Set<String> getSenderIds() {
     return senderIds;
-  }
-
-  protected Matcher createMatcher(SmsMessage message) {
-    return messagePattern.matcher(message.getBody());
-  }
-
-  protected boolean matchesPattern(Matcher matcher) {
-    return matcher.lookingAt();
   }
 }
